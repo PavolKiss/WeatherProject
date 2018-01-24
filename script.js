50949125941427eba661895521e0c1b214deca60
PavolKiss <pavol.kiss@akademiasovy.sk>
2018-01-24 19:31:52 +0100

    fixed button get data & added file jquery

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 $(document).ready(function() {
-	$("#getdt").click(function(){
+  $("#btnOk").click(function(){
+    var city=$("#city").val();
+    var code=$("#code").val();
+    if(city.length>1){
+      var urllink='http://api.openweathermap.org/data/2.5/weather?q=';
+      urllink=urllink + city;
+      if(code.length==2){
+           urllink=urllink+','+code;
+      }
+      urllink=urllink+'&appid=3bcfc1bb74a376edc543221f09e273b9';
 
-		var city = $("#city").val();
-		var code = $("#code").val();
-		if (city.length>1) {
-			var urllink='api.openweathermap.org/data/2.5/weather?q=';
-				urllink=urllink + city;
-			if (code.length==2){
-				urllink=urllink+','+code;
-			}
-			urllink=urllink+'&appid=3bcfc1bb74a376edc543221f09e273b9';
-			console.log(urllink);
-		}
+       $.ajax({
+         url: urllink,
+         data : { format: 'json' } ,
+         error : function(){
+         // sem sa vypise chyba
+         },
+         dataType: 'json',
+         success : function(data){
+            console.log("temp:"+data.main.temp);
+            console.log("desc:"+data.weather[0].description);
+         },
+         type: 'GET'
 
-		$.ajax({
-			url: urllink,
-			data : ( format: 'json' );
-			error : function(){
-				//vypis chyby
-			},
-			dataType: 'json',
-			sucess: function(data){
-				console.log("temp:"+data.main.temp);
-				console.log("desc:"+data.weather[0].description);
-			},
-			type: 'GET'
-		});
-})
+      });
 
-});	
+    }
+  });
+
+});
+
